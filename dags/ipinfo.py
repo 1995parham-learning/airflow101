2018d9bd62d7a4d1c9a3a81ad59baad7f787de03
Parham Alvani <parham.alvani@gmail.com>
2022-08-28 04:48:45 +0430

    feat: event better argument passing

--- dags/ipinfo.py
+++ dags/ipinfo.py
@@ -12,9 +12,7 @@ dag = DAG(
 )
 
 
-def _ipconfig(templates_dict):
-    output_file_name = templates_dict["output_file_name"]
-
+def _ipconfig(output_file_name: str):
     resp = requests.get("https://ipconfig.io/json")
     resp.raise_for_status()
     info: Dict[str, Any] = resp.json()
@@ -26,6 +24,6 @@ def _ipconfig(templates_dict):
 ipconfig = PythonOperator(
     task_id="request_ip_country",
     python_callable=_ipconfig,
-    templates_dict={"output_file_name": "/outputs/{{ ts_nodash }}.txt"},
+    op_kwargs={"output_file_name": "/outputs/{{ ts_nodash }}.txt"},
     dag=dag,
 )
